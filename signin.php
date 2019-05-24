498865507030c9fd5242fb1048971f0209f0af47
todatoday <46743825+todatoday@users.noreply.github.com>
2019-05-25 01:32:20 +0200

    Create signin.php

---
+++ signin.php
@@ -0,0 +1,30 @@
+<!DOCTYPE <!DOCTYPE html>
+
+    <head>
+        <meta charset="utf-8">
+       
+        <title>SignUp</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="signIn.css">
+      
+    </head>
+    <body>
+
+    <form action="signIn_process.php"  class="form" method="post">
+        <label for="user">Username Or email
+            <input type="text" name="user" id="user" required>
+        </label><br/>
+       
+        <label for="password">Password
+            <input type="password" name="password"  id="password" required>
+        </label><br/>
+      
+        <input type="submit" id="up" value="Sign Ip">
+
+
+    </form>
+      
+        
+    </body>
+</html>
